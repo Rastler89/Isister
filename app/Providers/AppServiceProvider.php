4e5e164ab1668e9e3c4c6cda7d6f459fdf1eae77
RastlerNomens <rastlerxiii@gmail.com>
2023-03-14 22:59:46 +0100

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind('path.public', function() {
-            return base_path('www');
+            return realpath(base_path().'/www');
         });
     }
 
