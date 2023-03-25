0f340069b0aef838f1ed9fd7fb95993231c94bcd
RastlerNomens <rastlerxiii@gmail.com>
2023-03-25 11:02:14 +0100

    revisado traducciones

--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -3,14 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Session;
+use App;
+use Config;
 use Illuminate\Http\Request;
 
 class Locale {
 
     public function handle(Request $request, Closure $next) {
-        $userLocale = $request->getPreferredLanguage(config('locales'));
+        if (Session::has('locale')) {
+            $locale = Session::get('locale', Config::get('app.locale'));
+        } else {
+            $locale = substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
 
-        app()->setLocale($userLocale);
+            if ($locale != 'en' && $locale != 'es') {
+                $locale = 'en';
+            }
+        }
+
+        App::setLocale($locale);
 
         return $next($request);
     }
